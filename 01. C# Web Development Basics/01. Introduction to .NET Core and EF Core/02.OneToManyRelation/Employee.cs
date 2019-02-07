248b6996d840d67294a2eeeb169abf7a72bacdd5
GHorozov <georgihorozov@hotmail.com>
2019-02-07 19:15:08 +0200

    Edit  task number

---
+++ 01. C# Web Development Basics/01. Introduction to .NET Core and EF Core/02.OneToManyRelation/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace _2.OneToManyRelation
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
