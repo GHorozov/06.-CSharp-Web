a319bf55e50c845fa1c76ad5dd4016666cbdd4b9
GHorozov <georgihorozov@hotmail.com>
2019-02-26 14:40:25 +0200

    Initial commit

---
+++ 01. C# Web Development Basics/03. HTTP Protocol/02.ValidateURL/StartUp.cs
@@ -0,0 +1,68 @@
+﻿namespace _02.ValidateURL
+{
+    using System;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var inputUrl = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(inputUrl))
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+
+            var decodedUrl = WebUtility.UrlDecode(inputUrl);
+            var parsedUrl = new Uri(decodedUrl);
+
+            
+            var isValidHostHttpsPort443 = parsedUrl.Scheme == "https" && parsedUrl.Port == 443;
+
+            if (parsedUrl.Scheme == "http"
+                && parsedUrl.Host != string.Empty
+                && parsedUrl.Port == 80
+                && parsedUrl.AbsolutePath != string.Empty
+                && parsedUrl.AbsolutePath.Contains("/"))
+            {
+                PrintParts(parsedUrl);
+            }
+            else if(parsedUrl.Scheme == "https"
+                && parsedUrl.Host != string.Empty
+                && parsedUrl.Port == 443
+                || parsedUrl.Port == 447
+                && parsedUrl.AbsolutePath != string.Empty
+                && parsedUrl.AbsolutePath.Contains("/"))
+            {
+                PrintParts(parsedUrl);
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+        }
+
+        private static void PrintParts(Uri parsedUrl)
+        {
+            Console.WriteLine($"Protocol: {parsedUrl.Scheme}");
+            Console.WriteLine($"Host: {parsedUrl.Host}");
+            Console.WriteLine($"Port: {parsedUrl.Port}");
+            Console.WriteLine($"Path: {parsedUrl.AbsolutePath}");
+
+            var query = parsedUrl.Query;
+            if (query != string.Empty)
+            {
+                Console.WriteLine($"Query: {query}");
+            }
+
+            var fragment = parsedUrl.Fragment;
+            if (fragment != string.Empty)
+            {
+                Console.WriteLine($"Fragment: {fragment}");
+            }
+        }
+    }
+}
---
+++ 01. C# Web Development Basics/03. HTTP Protocol/03.RequestParser/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace _03.RequestParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var lineParts = input.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var path = lineParts[0];
+                var method = lineParts[1];
+
+                dict[method] = path;
+            }
+
+            var inputRequest = Console.ReadLine().Split(new[] { "/", " "}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var methodRequest = inputRequest[0].ToLower();
+            var pathRequest = inputRequest[1];
+
+            Console.WriteLine(new string('-', 20));
+            if(dict.ContainsKey(methodRequest) && dict[methodRequest].Contains(pathRequest))
+            {
+                Console.WriteLine("HTTP/1.1 200 OK");
+                Console.WriteLine("Content-Lenght: 2");
+                Console.WriteLine("Content-Type: text/plain");
+                Console.WriteLine();
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("HTTP/1.1 404 NotFound");
+                Console.WriteLine("Content-Lenght: 9");
+                Console.WriteLine("Content-Type: text/plain");
+                Console.WriteLine();
+                Console.WriteLine("NotFound");
+            }
+        }
+    }
+}
