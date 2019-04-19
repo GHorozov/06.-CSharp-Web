fe9524a62ad43ed9c12274690a23a72205ba9896
GHorozov <georgihorozov@hotmail.com>
2019-04-19 18:11:14 +0300

    Initial commit

---
+++ 01. C# Web Development Basics/04. Web Server - Asynchronous Processing/04. HTML-Pages-Exersice-2/Launcher/ByTheCakeApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace MyCoolWebServer.ByTheCakeApplication.Controllers
+{
+    using MyCoolWebServer.ByTheCakeApplication.Infrastructure;
+    using MyCoolWebServer.Server.Http.Contracts;
+
+    public class HomeController : Controller
+    {
+        public IHttpResponse Index()
+        {
+            return this.FileViewResponse("index");
+        }
+
+        public IHttpResponse About()
+        {
+            return this.FileViewResponse("about");
+        }
+    }
+}
---
+++ 01. C# Web Development Basics/04. Web Server - Asynchronous Processing/04. HTML-Pages-Exersice-2/Launcher/ByTheCakeApplication/Infrastructure/Controller.cs
@@ -0,0 +1,18 @@
+﻿namespace MyCoolWebServer.ByTheCakeApplication.Infrastructure
+{
+    using MyCoolWebServer.ByTheCakeApplication.Views;
+    using MyCoolWebServer.Server.Enums;
+    using MyCoolWebServer.Server.Http.Contracts;
+    using MyCoolWebServer.Server.Http.Response;
+    using System.IO;
+
+    public abstract class Controller
+    {
+        public IHttpResponse FileViewResponse(string fileName)
+        {
+            var fileHtml = File.ReadAllText($@"C:\GitHub\06.-CSharp-Web\01. C# Web Development Basics\04. Web Server - Asynchronous Processing\04. HTML-Pages-Exersice-2\Launcher\ByTheCakeApplication\Resources\{fileName}.html");
+
+            return new ViewResponse(HttpStatusCode.Ok, new FileView(fileHtml));
+        }
+    }
+}
