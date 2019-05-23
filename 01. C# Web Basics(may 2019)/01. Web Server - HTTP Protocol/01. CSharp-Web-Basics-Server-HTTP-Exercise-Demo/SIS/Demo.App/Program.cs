4e1112e30c9338e7ca9f35edac47c9897bcc08eb
GHorozov <georgihorozov@hotmail.com>
2019-05-23 18:10:58 +0300

    Initial commit

---
+++ 01. C# Web Basics(may 2019)/01. Web Server - HTTP Protocol/01. CSharp-Web-Basics-Server-HTTP-Exercise-Demo/SIS/Demo.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using Demo.App.Controllers;
+using SIS.HTTP.Enums;
+using SIS.WebServer;
+using SIS.WebServer.Result;
+using SIS.WebServer.Routing;
+using SIS.WebServer.Routing.Contracts;
+
+namespace Demo.App
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/", httpRequest 
+                => new HomeController().Home(httpRequest));
+
+            Server server = new Server(8000, serverRoutingTable);
+            server.Run();
+        }
+    }
+}
