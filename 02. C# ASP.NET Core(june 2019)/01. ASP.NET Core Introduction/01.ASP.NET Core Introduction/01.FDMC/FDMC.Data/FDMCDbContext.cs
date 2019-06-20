4c6e5f98107458e03f1811a9b62874337709ad13
GHorozov <georgihorozov@hotmail.com>
2019-06-20 22:16:33 +0300

    Initial commit

---
+++ 02. C# ASP.NET Core(june 2019)/01. ASP.NET Core Introduction/01.ASP.NET Core Introduction/01.FDMC/FDMC.Data/FDMCDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FDMC.Data
+{
+    public class FDMCDbContext : DbContext
+    {
+        public FDMCDbContext(DbContextOptions<FDMCDbContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ 02. C# ASP.NET Core(june 2019)/01. ASP.NET Core Introduction/01.ASP.NET Core Introduction/02.Panda.App/02.Panda.App/Startup.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Panda.Data;
+
+namespace _02.Panda.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PandaDbContext>(options => 
+            options.UseSqlServer(this.Configuration["ConnectionString:DefaultConnection:"]));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+    }
+}
