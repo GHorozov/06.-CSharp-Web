0d7a37e273e9ec7f49429e9f7ef488a00411ce71
GHorozov <georgihorozov@hotmail.com>
2020-04-09 13:35:58 +0300

    seed categories

---
+++ 03.Workshop-Forum(april 2020)/Forum.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,32 @@
+﻿namespace Forum.Data.Seeding
+{
+    using Forum.Data.Seeding.Interfaces;
+    using Forum.DataModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ForumDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            var categories = new List<string>() { "Sport", "Coronavirus", "News", "Programming", "Cats", "Dogs", "Arts" };
+            foreach (var category in categories)
+            {
+                await dbContext.Categories.AddAsync(new Category()
+                {
+                    Name = category,
+                    CreatedOn = DateTime.UtcNow,
+                    Description = category,
+                    Title = category,
+                });
+            }
+        }
+    }
+}
